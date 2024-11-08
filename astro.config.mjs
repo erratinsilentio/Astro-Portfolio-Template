853fcccda9323ee655e44af1239f166c457d4cdf
erratinsilentio <kacper.kondracki@wp.pl>
2024-11-08 22:55:24 +0100

    added shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import solidJs from '@astrojs/solid-js';
+import solidJs from "@astrojs/solid-js";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind({ applyBaseStyles: false }), solidJs(), react()],
 });
