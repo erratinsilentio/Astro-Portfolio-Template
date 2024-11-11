e47be8db7d8a827dbc47f0d39850045c0a54ec20
erratinsilentio <kacper.kondracki@wp.pl>
2024-11-11 02:08:12 +0100

    reconstructed home page to use video as a bg

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,13 +1,14 @@
 export default function LogoHeader() {
   return (
     <div className=" flex flex-col items-center justify-center sm:flex-row sm:items-end">
-      <div className="flex flex-col items-center justify-center sm:flex-row sm:items-end">
-        <img src="/Ball.png" alt="Tarot Reader logo" width={130} />
+      <div className="flex flex-col items-center justify-center sm:flex-row sm:items-center">
+        {/* <img src="/Ball.png" alt="Tarot Reader logo" width={130} /> */}
+        <span className="text-[120px] leading-8 mr-6">👾</span>
         <section className="">
-          <h1 className="px-4 tracking-wide text-center sm:text-left text-[44px] sm:text-[50px] font-[family-name:var(--font-geist-mono)] bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-300 to-indigo-300">
+          <h1 className=" tracking-wide text-center sm:text-left text-[44px] sm:text-[50px] font-[family-name:var(--font-geist-mono)] bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-300 to-indigo-300">
             erratinsilentio.dev
           </h1>
-          <span className=" text-[20px] text-center font-[family-name:var(--font-geist-mono)] bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-400 to-pink-500">
+          <span className=" text-[20px] text-center font-[family-name:var(--font-geist-mono)] bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-purple-500 to-purple-700">
             Creative Web Developer
           </span>
         </section>
--- src/components/home/instructions.tsx
+++ src/components/home/instructions.tsx
@@ -2,12 +2,17 @@ export default function Instructions() {
   return (
     <ul className="text-purple-200 pt-4 list-inside  text-sm text-center sm:text-left font-courier leading-6 tracking-tight">
       <li className="mb-2 flex items-center gap-3">
-        <span className="text-[22px]">⚜️</span>Working on the frontend but
-        interested in all kinds of tech
+        <span className="text-[22px] text-yellow-600">⚜️</span>Working on the
+        Frontend, interested in all kinds of tech
       </li>
-      <li className="mb-2 flex items-center gap-2">
-        <span className="text-[22px]">⚜️</span>Im always ready to put in the
-        work - smart & hard
+      <li className="mb-2 flex items-center gap-3">
+        <span className="text-[22px] text-yellow-600">⚜️</span>Always ready to
+        put in the work - both{" "}
+        <span className="underline tracking-wider ring-offset-amber-800">
+          smart
+        </span>
+        <span className="text-[18px] font">&</span>
+        <span className="underline tracking-wider">hard</span>
       </li>
     </ul>
   );
